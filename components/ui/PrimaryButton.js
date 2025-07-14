2d33e842047f1c6d49f340b01c1f1303aefe804e
Mahsa Fateh <mahsafaateh@gmail.com>
2025-07-14 17:00:05 +0200

    adding image and game over screen style

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,44 +1,44 @@
-import { Pressable, Text, StyleSheet, View } from "react-native";
+import {Pressable, StyleSheet, Text, View} from "react-native";
 
-export default function PrimaryButton({ children, onPress }) {
+export default function PrimaryButton({children, onPress}) {
 
-  return (
-    <View style={styles.viewContainer}>
-      <Pressable
-        onPress={onPress}
-        style={({ pressed }) =>
-          pressed
-            ? [styles.pressableContainer, styles.pressed]
-            : styles.pressableContainer
-        }
-        android_ripple={{ color: "#BB9AB1" }}
-      >
-        <Text style={styles.text}>{children}</Text>
-      </Pressable>
-    </View>
-  );
+    return (
+        <View style={styles.viewContainer}>
+            <Pressable
+                onPress={onPress}
+                style={({pressed}) =>
+                    pressed
+                        ? [styles.pressableContainer, styles.pressed]
+                        : styles.pressableContainer
+                }
+                android_ripple={{color: "#BB9AB1"}}
+            >
+                <Text style={styles.text}>{children}</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  viewContainer: {
-    flex: 1,
-    borderRadius: 28,
-    margin: 4,
-    overflow: "hidden",
-  },
-  pressableContainer: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    backgroundColor: "#BB8493",
-    elevation: 2,
-  },
-  text: {
-    textAlign: "center",
-    fontWeight: "semibold",
-    fontSize: 15,
-    color: "#EEEEEE",
-  },
-  pressed: {
-    opacity: 0.75,
-  },
+    viewContainer: {
+        flex: 1,
+        borderRadius: 28,
+        margin: 4,
+        overflow: "hidden",
+    },
+    pressableContainer: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        backgroundColor: "#BB8493",
+        elevation: 2,
+    },
+    text: {
+        textAlign: "center",
+        fontWeight: "semibold",
+        fontSize: 15,
+        color: "#EEEEEE",
+    },
+    pressed: {
+        opacity: 0.75,
+    },
 });
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,63 @@
-import { View, Text } from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Title from "../components/ui/Title";
+import Colors from "../utils/colors";
 
 function GameOverScreen() {
-  return (
-    <View>
-      <Text>Game is OVER now.</Text>
-    </View>
-  );
+    return (
+        <View style={styles.rootContainer}>
+            <Title>GAME OVER!</Title>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={require('../assets/images/success.png')}
+                />
+            </View>
+            <Text style={styles.summaryText}>
+                Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+                guess the number <Text style={styles.highlight}>Y</Text>.
+            </Text>
+            <View style={styles.btnContainer}>
+                <PrimaryButton>Start New Game</PrimaryButton>
+            </View>
+        </View>
+    );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    summaryText: {
+        fontFamily: 'open-sans',
+        fontSize: 24,
+        textAlign: 'center',
+        marginBottom: 24
+    },
+    highlight: {
+        fontFamily: 'open-sans-bold',
+        color: Colors.primary500,
+    },
+    btnContainer: {
+        width: 180,
+        height: 40,
+    }
+});
